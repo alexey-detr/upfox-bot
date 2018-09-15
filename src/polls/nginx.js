378a215953abb753f752c1c6616b469096d2867d
Alexey Ponomarev <xlz@ya.ru>
2018-09-16 02:02:16 +0700

    Updated nginx poller config

--- src/polls/nginx.js
+++ src/polls/nginx.js
@@ -1,8 +1,6 @@
-'use strict';
-
 module.exports = {
     name: 'nginx',
     url: 'http://nginx.org/',
     urlDownload: 'http://nginx.org/en/download.html',
-    pattern: /nginx\-([0-9\.]+)/,
+    pattern: /nginx-([0-9.]+)/,
 };
