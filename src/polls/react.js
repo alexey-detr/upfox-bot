1fd9a16036a728d61a29e343fe353e8d974cc31c
Alexey Ponomarev <xlz@ya.ru>
2018-09-16 02:11:00 +0700

    Added React.js poller

---
+++ src/polls/react.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'React',
+    url: 'https://github.com/facebook/react/releases.atom',
+    urlDownload: 'https://reactjs.org/',
+    pattern: /releases\/tag\/v([0-9.]+)"/,
+};
