51cda8a6f45292d9e61c7189d1a87c58f0246a26
Alexey Ponomarev <xlz@ya.ru>
2018-09-16 11:52:06 +0700

    Added NPM poller

---
+++ src/polls/npm.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'NPM',
+    url: 'https://github.com/npm/cli/releases.atom',
+    urlDownload: 'https://www.npmjs.com/',
+    pattern: /\/npm\/cli\/releases\/tag\/v([0-9.]+)"/,
+};
