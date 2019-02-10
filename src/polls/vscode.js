ffa5db6d468f99e5c259e55128352f402e1cdc11
Alexey Ponomarev <xlz@ya.ru>
2019-02-11 00:41:38 +0700

    Fixed vscode poller

--- src/polls/vscode.js
+++ src/polls/vscode.js
@@ -2,5 +2,5 @@ module.exports = {
     name: 'Visual Studio Code',
     url: 'https://github.com/Microsoft/vscode/releases.atom',
     urlDownload: 'https://code.visualstudio.com/',
-    pattern: /\/Microsoft\/vscode\/releases\/tag\/v([0-9.]+)/,
+    pattern: /\/Microsoft\/vscode\/releases\/tag\/([0-9.]+)/,
 };
